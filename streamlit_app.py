283bd2ec1b399eed762a49b0eec21fd1ddaec385
afzal-comp-apps <80603500+afzal-comp-apps@users.noreply.github.com>
2021-12-04 08:21:11 -0500

    Adding first elements of WHInput

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,11 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 with st.echo(code_location='below'):
+    st.text_input('Project Name')
+    st.radio('Units', ['SI','English'])
+    st.number_input('Print transient conditions every __ seconds')
+
+    
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
