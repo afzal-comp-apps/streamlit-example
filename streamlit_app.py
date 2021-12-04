1974f308e1e4ee069a1654baab6ec23411237e37
afzal-comp-apps <80603500+afzal-comp-apps@users.noreply.github.com>
2021-12-04 08:24:38 -0500

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 with st.echo(code_location='below'):
     st.text_input('Project Name')
     st.radio('Units', ['SI','English'])
-    st.number_input('Print transient conditions every __ seconds')
+    st.number_input('Print transient conditions every __ seconds', minvalue=0, step=1)
 
     
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
